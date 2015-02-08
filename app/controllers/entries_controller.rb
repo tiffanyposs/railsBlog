ffd59c0c68fb92cc022206877e684c5b5d88fa76
tiffanyposs <tiffanyposs@gmail.com>
2015-02-07 20:26:26 -0500

    log in

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,28 +1,59 @@
 class EntriesController < ApplicationController
-	def index
-		@entries = Entry.all
-  	end
 
-  	def show
-  		@entry = Entry.find_by(id: params[:id])
-  	end
+ def index
+   @entries = Entry.all
+   @user = User.find_by(id: session[:user_id])
+ end
 
-    def new
-      @entry = Entry.new
-    end
+ def show
+  @entry = Entry.find_by(id: params[:id])
+  @user = User.find_by(id: session[:user_id])
+ end
 
-    def create
-      @entry = Entry.create(entry_params)
-      redirect_to @entry
-    end
+ def new
+   @user = User.find_by(id: session[:user_id])
+   if @user && @user.admin_status == true
+     @entry = Entry.new
+   else
+     redirect_to '/'
+   end
+ end
 
-    private
-      def entry_params
-      params.require(:entry).permit(:title, :content)
-    end
+ def create
+   @entry = Entry.create(entry_params)
+   redirect_to @entry
+ end
 
-  	# private
-  	#   def entry_params
-	  # params.require(:entry).permit(:title, :content)
-  	# end
+ def edit
+   @user = User.find_by(id: session[:user_id])
+   if @user && @user.admin_status == true
+     @entry = Entry.find_by(id: params[:id])
+   else
+     redirect_to '/'
+   end
+ end
+
+ def update
+   @entry = Entry.find_by(id: params[:id])
+   @entry.update_attributes(entry_params)
+   redirect_to @entry
+ end
+
+ def destroy
+   @user = User.find_by(id: session[:user_id])
+   if @user && @user.admin_status == true
+     @entry = Entry.find_by(id: params[:id])
+     @entry.destroy
+     redirect_to entries_path
+   else
+     redirect_to '/'
+   end
+ end
+
+ private
+ def entry_params
+   params.require(:entry).permit(:title, :content)
+ end
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+
+User.create(first_name: "Tiffany", last_name: "Poss", email: "tiffanyposs@gmail.com", username: 'tiffanyposs', password: 'tiffany', admin_status: true)
+
+User.create(first_name: "Karl", last_name: "Poss", email: "karlposs@gmail.com", username: 'karlposs', password: 'karl', admin_status: false)
+
